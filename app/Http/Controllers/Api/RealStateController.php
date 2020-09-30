738af10af51224d05ea56f7bf42f87fdd3a11b9b
cmunhozprog <cmunhozprog03@hotmail.com>
2020-09-30 16:34:01 -0300

    Aula 39 completa

---
+++ app/Http/Controllers/Api/RealStateController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\RealState;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RealStateController extends Controller
+{
+    /**
+     * @var RealState
+     */
+    private $realState;
+
+    public function __construct(RealState $realState)
+    {
+        $this->realState = $realState;
+    }
+
+    public function index()
+    {
+        $realState = $this->realState->paginate(10);
+
+        return response()->json($realState, 200);
+    }
+}
